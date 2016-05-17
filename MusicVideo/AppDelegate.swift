77731c663e848ac6dd4ca6f69a1362db2d779cf6
ecruz1542 <ecruz1542@gmail.com>
2016-05-17 19:59:47 -0400

    Completed Part 2 Coding Session

--- MusicVideo/AppDelegate.swift
+++ MusicVideo/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+       
+        //Turn off caching of NSURLSession
+//        NSURLCache.setSharedURLCache(NSURLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil))
+        
+        
         return true
     }
 
